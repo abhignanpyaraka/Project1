b8e36ae7213c691716eb6ec47f78b5e9f493206a
abhignan <abhignanacharya@gmail.com>
2017-05-30 18:48:23 +0530

    first project

---
+++ Project1/src/com/java/basics/DemoMaths.java
@@ -0,0 +1,12 @@
+package com.java.basics;
+
+public class DemoMaths {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		int j=20;
+		System.out.println(i+j);
+	}
+
+}
